e7c2f2c877dd6db8a66e49a21bb6bbb6d08dfcad
Piotr Marecki <pm@aimachina.io>
2020-06-24 18:33:00 +0200

    backend wersja działająca, 1.0

--- src/vote.js
+++ src/vote.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 2020; // default port to listen
+
+const {BAD_REQUEST, UNAUTHORIZED, INTERNAL_SERVER_ERROR} = require('http-status-codes');
+
 
 const Pool = require('pg').Pool;
 const pool = new Pool({
@@ -10,6 +12,125 @@ const pool = new Pool({
     user: 'student',
     password: 'wsiz#1234'
 });
+
+const NodeCache = require("node-cache");
+const user_cache = new NodeCache({stdTTL: 600});
+
+// const needle = require('needle');    //also nice, but doesn't have async/await
+const https = require("https");
+const tls_options = {
+    rejectUnauthorized: false  // verify SSL certificate
+}
+const agent = new https.Agent(tls_options);
+const fetch = require("node-fetch");
+//^^ using `node-fetch` for async operations
+
+// import {v4 as uuidv4} from 'uuid';
+const {v4: uuidv4} = require('uuid');//https://github.com/uuidjs/uuid
+
+const PORT = 2020;
+const AUTH = 'https://denver.wsi.edu.pl:8443/wd-auth';
+
+
+/**
+ * Sends an error response to the client:
+ * @param res
+ * @param message
+ * @param error_code
+ * @returns {void}
+ */
+async function send_error_response(res, message, error_code) {
+    res.status(error_code);
+    await res.send({"error": message});
+}
+
+async function send_ok_response(res, message = 'OK') {
+    await res.send({"result": message});
+}
+
+
+/**
+ * Logownie do bazy userów (tutaj: WD)
+ * @param {string} user
+ * @param {string} md5pass
+ * @returns {string}
+ */
+async function login(user, md5pass) {
+    let url = `${AUTH}/auth?album=${user}&pass=${md5pass}`
+    let res = await fetch(url, {agent});
+    console.log(`res=${res}`)
+    let authtoken = (await res.text()).substr(1, 36);
+    console.log(`userid=${user} logged in, authtoken:${authtoken}`);
+    return authtoken;
+}
+
+/**
+ * Sprawdzenie jaki userid odpowiada podanemu authtoken. Ta funkcja powinna być
+ * obsługiwana przez bazę userów (tutaj: WD).
+ * Wykorzystamy bibliotekę "node-cache" by wyniki tej operacji zapamiętać (nie pytać ciągle bazy userów).
+ *
+ * @param authtoken
+ * @returns {string} (undefined jeśli authtoken nie odpowiada żadnemu userowi)
+ */
+async function get_userid(authtoken) {
+    if (user_cache.has(authtoken)) {
+        let userid = user_cache.get(authtoken);
+        console.log(`returning cached user: ${userid}`);
+        return user_cache.get(authtoken);
+    } else {
+        //ask user DB
+        let url = `${AUTH}/user?wdauth=${authtoken}`
+        let res = await fetch(url, {agent});
+        let data = await res.json();
+        let uuid = data.studentid;
+        console.log(`WD: userid=${uuid}`);
+        user_cache.set(authtoken, uuid);
+        return uuid;
+    }
+}
+
+/**
+ * Zwraca wybory dostępne dla podanego usera. Wybory muszą być:
+ * - otwarte dla danego timestamp-u (po votebegin, i przed voteend),
+ * - userid nie może być jeszcze zarejestrowany do danych wyborów (tabela registrations)
+ *
+ * Uwaga -- timestamp-y są zone-naive (unaware); zakładamy, że wszystkie są w tej samej strefie
+ * czasowej (np. CEST).
+ *
+ * @param userid
+ * @param at_timestamp
+ * @returns {Map[]}
+ */
+async function get_active_unregistered_elections(userid, at_timestamp) {
+    const {rows} = await pool.query(
+            `select *
+             from v2.elections e
+             where $2 between e.votebegin and e.voteend
+               and e.electionid not in (
+                 select r.electionid
+                 from v2.registrations r
+                 where userid = $1)`,
+        [userid, at_timestamp]);
+    return rows;
+}
+
+
+/**
+ * Sprawdzenie wyników głosowania, czy są zgodne z regułami. W tej wersji, wszystkie ".value" mają
+ * być dodatnie, i co najwyżej jeden może być ==1. Dodaktowo electionid musi się zgadzać z podanym `eid`.
+ *
+ * @param {string} eid : electionid dla którego obecne głosy mają być odpowiednie
+ * @param {Map[]} votes pojedynczy wynik głosowania (de facto tabela obiektów typu Vote)
+ */
+function isvalid_v1(votes, eid) {
+    let total = 0;
+    for (const v of votes) {
+        if (v.value < 0) return false;
+        total += v.value;
+        if (total > 1) return false;
+    }
+    return true;
+}
 
 app.use(express.json());        //pozwala na czytanie req.body
 
@@ -27,13 +148,104 @@ app.get("/", (req, res) => {
     res.send(r);
 });
 
+app.get("/login", async (req, res) => {
+    let user, pass, authtoken;
+    try {
+        user = req.query.user;
+        pass = req.query.pass;
+    } catch (e) {
+        return send_error_response(res,
+            'User or password not provided', BAD_REQUEST);
+    }
+    try {
+        authtoken = await login(user, pass);
+    } catch (e) {
+        return send_error_response(res, 'Unauthorized', UNAUTHORIZED);
+    }
+    if (authtoken.indexOf('FAILED') !== -1) {
+        return send_error_response(res, 'Unauthorized', UNAUTHORIZED);
+    }
+    await res.send({"authtoken": authtoken});
+});
 
-app.get("/choices", (req, res) => {
+
+app.get("/user", async (req, res) => {
+    let authtoken = req.query.authtoken;
+    let user = await get_userid(authtoken);
+    await res.send({"userid": user});
+});
+
+
+app.get("/elections", async (req, res) => {
+    let authtoken;
+    try {
+        authtoken = req.query.authtoken;
+    } catch (e) {
+        return send_error_response(res, 'Missing authtoken', BAD_REQUEST);
+    }
+    let userid = await get_userid(authtoken);
+    if (userid === undefined) {
+        return send_error_response(res, 'Bad authtoken', BAD_REQUEST);
+    }
+    let ee = await get_active_unregistered_elections(userid, new Date());
+    await res.send({"elections": ee});
+});
+
+
+app.get('/register', async (req, res) => {
+    let authtoken, electionid;
+    try {
+        authtoken = req.query.authtoken;
+        electionid = req.query.electionid;
+        electionid = parseInt(electionid);
+    } catch (e) {
+        return send_error_response(res, 'Missing authtoken or electionid', BAD_REQUEST);
+    }
+    let userid = await get_userid(authtoken);
+    if (userid === undefined) {
+        return send_error_response(res, 'Bad authtoken', BAD_REQUEST);
+    }
+    let allowed_elections = await get_active_unregistered_elections(userid, new Date());
+    let allowed_electionids = allowed_elections.map(e => e.electionid);
+    if (!allowed_electionids.includes(electionid)) {
+        return send_error_response(res, `Given electionid=${electionid} is not allowed`, BAD_REQUEST);
+    }
+
+    //W tym momencie mamy ważną, dopuszczoną parę (userid, electionid)
+    //(trzeba uważać, bo w przypadku ataku, wiele requestów może dojść do tego miejsca, z tą samą parą)
+
+    //transaction
+    // - generate UUID
+    // - write to "tokens"
+    // - write to "registrations"
+    // - return token
+
+    let token = uuidv4();
+    try {
+        await pool.query('BEGIN')
+
+        const writetoken = 'INSERT INTO v2.tokens(electionid, token) VALUES($1, $2)';
+        await pool.query(writetoken, [electionid, token]);
+
+        const writeregistration = 'INSERT INTO v2.registrations(userid, electionid) VALUES ($1,$2)';
+        await pool.query(writeregistration, [userid, electionid]);
+
+        await pool.query('COMMIT') //tylko 1 request może dojść do tego miejsca
+
+    } catch (e) {
+        await pool.query('ROLLBACK');
+        console.log(`Error of token generation for userid=${userid} and electionid=${electionid}`);
+        return send_error_response(res, 'Error in election_token generation', INTERNAL_SERVER_ERROR);
+    }
+    console.log(`Token generated for userid=${userid} and electionid=${electionid}`);
+    res.send({'election_token': token});
+});
+
+
+app.get('/choices', (req, res) => {
     let electionid = req.query.electionid;
     if (electionid === undefined) {
-        res.statusCode = 400;     //bad request
-        res.send({"error": "electionid missing"});
-        return;
+        return send_error_response(res, 'Missing electionid', BAD_REQUEST);
     }
     pool.query('select * from v2.choices  where electionid=$1 order by title', [electionid],
         (er, re) => {
@@ -43,78 +255,58 @@ app.get("/choices", (req, res) => {
 });
 
 
-
 app.post("/vote", async (req, res) => {
-    let bb = req.body;  //is an array of `votes`
-    console.log(`zapisujemy ${JSON.stringify(bb)}`);
-    for (const v of bb) {
-        console.log(`rekord: ${JSON.stringify(v)}, ${new Date().getTime()}`)
-        const {rows} = await pool.query('' +
-            'insert into v2.votes(electionid, choiceid, value) values ($1, $2, $3)',
-            [v.electionid, v.choiceid, v.value]);
+    let votes = req.body;  //is an array of `votes`
+    let token;
+    try {
+        token = req.query.election_token;
+    } catch (e) {
+        return send_error_response(res, 'Missing election_token', BAD_REQUEST);
     }
-    res.send({"result": "OK"});
-});
+    console.log(`token:${token} submited vote ${JSON.stringify(votes)}`);
+
+    //Znajdźmy electionid odpowiadające podanemu tokenowi
+    const eid = await pool.query('SELECT electionid from v2.tokens where token=$1', [token]);
+    console.log(`token ${token} valid for election_id=${eid}`);
 
 
+    if (!isvalid_v1(votes, eid)) {
+        return send_error_response(res, 'invalid vote', BAD_REQUEST);
+    }
 
+    //Mamy token i głosy; głosy trzeba zapisać, a token usunąć (najlepiej w 1 transakcji)
+    try {
+        await pool.query('BEGIN')
 
-//Przykład: insert elementu (dane przyjdą w request body)
-//request można wysłać np. z aplikacji postman, albo z dowolnej własnej aplikacji intrnetowej
-app.post("/cards/insert", (req, res) => {
-    //todo: admin guard
-    let bb = req.body;
-    console.log(`Żądanie zapisu: ${JSON.stringify(bb)}`);
-    pool.query('insert into d1.card (categoryid, title, text, url) values ($1,$2,$3,$4) returning *',
-        [bb.categoryid, bb.title, bb.text, bb.url],
-        (er, re) => {
-            if (er) throw er;
-            res.send(re.rows[0]);
-        });
-});
+        //Token musi zostać unieważniony; (nowego dla danego usera już się nie wygeneruje)
+        let resultset = await pool.query('DELETE FROM v2.tokens where token=$1 returning *', [token]);
+        let deleted_count = resultset.rowCount;
+        console.log(`deleted tokens: ${deleted_count}`);
+        if (deleted_count !== 1) {
+            console.log(`Vote manipulation detected for token ${token};`);
+            throw new Error('invalid vote');
+        }
 
+        //Zapis każdego z głosów
+        for (const v of votes) {
+            console.log(`vote to save: ${JSON.stringify(v)}`);
+            await pool.query('INSERT INTO v2.votes(electionid, choiceid, value) VALUES ($1, $2, $3)',
+                [v.electionid, v.choiceid, v.value]);
+        }
 
-//Przykład modyfikacji istniejących danych; ważne, że przychodzący obiekt (w request.body) ma wypełnione pole `id`
-app.post("/cards/update", (req, res) => {
-    //todo: admin guard
-    let bb = req.body;
-    console.log(`Żądanie aktualizacji: ${JSON.stringify(bb)}`);
-    pool.query('update d1.card set categoryid=$1, title=$2, text=$3, url=$4 where id=$5',
-        [bb.categoryid, bb.title, bb.text, bb.url, bb.id], (er, re) => {
-            if (er) throw er;
-            res.send({"result": "OK"});
-        });
-});
+        await pool.query('COMMIT')
 
-//usuwanie rekordów z bazy
-app.delete("/cards/delete", (req, res) => {
-    //todo: admin guard
-    let id = req.query.id;
-    console.log(`Żądanie usunięcia rekordu o id: ${id}`);
-    pool.query('delete from d1.card where id=$1',
-        [id], (er, re) => {
-            if (er) throw er;
-            res.send({"result": "OK"});
-        });
-});
+    } catch (e) {
+        await pool.query('ROLLBACK');
+        console.log(`Error of vote saving for token ${token}`);
+        return send_error_response(res, 'Error of vote saving', INTERNAL_SERVER_ERROR);
+    }
 
-
-//All CRUD for /cards implemented
-
-
-app.get("/add", (req, res) => {
-    console.log(`zapytanie http o /add`);
-    let a = req.query.a;
-    let b = req.query.b;
-    a = parseInt(a);
-    b = parseInt(b);
-
-    res.send(`wynik dodawania ${a} + ${b} = ${a + b}`);
+    res.send({"result": 'OK'});
 });
 
 
 // start the Express server
-app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
-    pool.query('set search_path to xxx');
+app.listen(PORT, () => {
+    console.log(`server started at http://localhost:${PORT}`);
 });
