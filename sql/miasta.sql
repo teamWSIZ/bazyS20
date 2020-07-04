8ee64e7b9dab49366d57c0bf3978236f1601c645
Piotr Marecki <pm@aimachina.io>
2020-07-04 10:57:32 +0200

    tabela miast -- DDL

---
+++ sql/miasta.sql
@@ -0,0 +1,15 @@
+create table cities
+(
+	id serial not null
+		constraint nodes_pkey
+			primary key,
+	pid integer default 0,
+	name text default ''::text,
+	pop double precision default 1,
+	leaf boolean default true,
+	x double precision default 0.0,
+	y double precision default 0.0
+);
+
+
+
